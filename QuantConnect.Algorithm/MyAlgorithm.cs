a1dc4825b085c70a719a0b9c921428e3d1b92de9
Jared Broad <jared@quantconnect.com>
2013-06-08 21:38:12 -0400

    Renamed projects and Added example Algo template

---
+++ QuantConnect.Algorithm/MyAlgorithm.cs
@@ -0,0 +1,59 @@
+﻿/*
+* QUANTCONNECT.COM - 
+* QC.Algorithm - Example starting point for a locally compiled user algorithm
+*/
+
+/**********************************************************
+* USING NAMESPACES
+**********************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+using QuantConnect.Securities;
+using QuantConnect.Models;
+
+namespace QuantConnect {
+
+    /******************************************************** 
+    * CLASS DEFINITIONS
+    *********************************************************/
+    /// <summary>
+    /// Example user algorithm class - if you can code 
+    /// </summary>
+    public class MyAlgorithm : QCAlgorithm {
+
+        /******************************************************** 
+        * CLASS METHODS
+        *********************************************************/
+        
+        /// <summary>
+        /// Initialise your algorithm here.
+        /// </summary>
+        public override void Initialize() {
+            //Configure Start and End Date:
+            SetStartDate(2012, 01, 01);
+            SetEndDate(2012, 12, 31);
+            //Request IBM Data
+            AddSecurity(SecurityType.Equity, "IBM", Resolution.Minute, true, false);
+            //Set your starting capital:
+            SetCash(50000);
+        }
+
+        /// <summary>
+        /// Handle your tradeBar's here.
+        /// </summary>
+        /// <param name="symbols">Dictionary of data objects</param>
+        public override void OnTradeBar(Dictionary<string, TradeBar> data) {
+            //Use the data to generate orders.
+            if (!Portfolio.HoldStock)
+            {
+                Order("IBM", 10, OrderType.Market);
+            }
+        }
+
+    } // End Algorithm Template
+
+} // End QC Namespace
